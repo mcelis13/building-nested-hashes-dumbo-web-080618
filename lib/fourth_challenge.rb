6e0f9da81042bb5fa21405ab9e2ac8ba1835bbc0
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 21:24:34 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -21,6 +21,6 @@ def fourth_challenge
 }
 end
 
-epic_tragedy[:montague][:hero]['status'] = 'dead'
+#epic_tragedy[:montague][:hero]['status'] = 'dead'
 
-epic_tragedy[:capulet][:heroine]['status'] = 'dead'
+#epic_tragedy[:capulet][:heroine]['status'] = 'dead'
